cf861f2160de089fcde3ca563f81766210f6ee2e
studioshane <studioshane@gmail.com>
2018-12-21 17:27:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(i)
+
+  outliers = [-1,0,1]
+
+    if outliers.include?(i)
+      return false
+    else
+
+      (2...i).each { |x|
+        if i % x == 0
+          return false
+        end
+      }
+    end
+    true
+ end
